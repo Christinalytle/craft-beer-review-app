440e278c9f726cd3ce7fdf3804629f4ccb27eb3f
Christina Lytle <christina.lytle17@gmail.com>
2024-04-29 14:22:41 -0700

    LWC

--- force-app/main/default/lwc/beerCompanyLWC/beerCompanyLWC.js
+++ force-app/main/default/lwc/beerCompanyLWC/beerCompanyLWC.js
@@ -1,3 +1,55 @@
 import { LightningElement } from 'lwc';
 
-export default class BeerCompanyLWC extends LightningElement {}
+export default class BeerCompanyLWC extends LightningElement {
+    city;
+    get city() {
+        return this.city;
+    }
+    set city(value) {
+        this.city = value;
+    }
+  
+    breweries = [];
+    get breweries() {
+        return this.breweries;
+    }
+    set breweries(value) {
+        this.breweries = value;
+    }
+   
+    error;
+    get error() {
+        return this.error;
+    }
+    set error(value) {
+        this.error = value;
+    }
+
+    handleInputChange(event) {
+        this.city = event.target.value;
+    }
+
+    handleSearch() {
+        if (!this.city.trim()) {
+            this.error = 'Please enter a city name.';
+            return;
+        }
+
+        this.error = '';
+
+        fetch(`https://api.openbrewerydb.org/v1/breweries?by_city=${this.city}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                this.breweries = data;
+            })
+            .catch(error => {
+                this.error = 'Error fetching breweries: ' + error.message;
+            });
+    }
+
+}
